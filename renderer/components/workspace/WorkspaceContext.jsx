9b1605593fb185b458eca7e62b94327803e0dfbb
Nicolas Longchamps <lonn1001@usherbrooke.ca>
2023-06-27 01:07:59 -0400

    Progress on the input module and on the workspace integration between Electron and Next

---
+++ renderer/components/workspace/WorkspaceContext.jsx
@@ -0,0 +1,34 @@
+import { React, useState, useContext, createContext } from 'react';
+
+/**
+ * @typedef {React.Context} WorkspaceContext
+ * @description 
+ * @summary 	
+ * @see 
+ */
+export const WorkspaceContext = createContext(null);
+
+/**
+ * @typedef {React.FunctionComponent} WorkspaceProvider
+ * @description This component provides the context for the workspace. It will provide the workspace object and the setWorkspace function to the children.
+ * @params {Object} children -
+ * @params {Object} workspaceObject -
+ * @summary The workspace object is the object that contains the workspace information. It is an object that contains the following properties:'hasBeenSet' and 'workspaceObject'.
+ *          The 'hasBeenSet' property is a boolean that indicates if the workspace has been set. The 'workspaceObject' property is the workspace containing information about all the files and folders in the workspace.
+ *  
+ */
+export default function WorkspaceProvider({ children }) {
+
+    const [workspace, setWorkspace] = useState({hasBeenSet: false, workspaceObject: null});
+
+    return (
+        <>
+            <WorkspaceContext.Provider value={{ workspace, setWorkspace }}>
+                {children}
+            </WorkspaceContext.Provider>
+        </>
+    );
+}
+
+
+
