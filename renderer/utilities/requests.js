b22520f53b9b8335e164f0b7e7f6076d2ed28f19
AndreanneAllaire <andreanne.allaire@usherbrooke.ca>
2023-07-06 11:40:43 -0400

    Added post function using axios in utilities.

--- renderer/utilities/requests.js
+++ renderer/utilities/requests.js
@@ -1,24 +1,43 @@
-import { ipcRenderer } from "electron"
+import { ipcRenderer } from "electron";
+import axios from "axios";
 
+export const requestJson = (
+  port,
+  topic,
+  json2send,
+  jsonReceivedCB,
+  errorCB
+) => {
+  ipcRenderer
+    .invoke("request", {
+      data: {
+        json2send,
+      },
+      method: "POST",
+      url: "http://127.0.0.1:" + port + "/" + topic,
+    })
+    .then((data) => {
+      jsonReceivedCB(data["data"]);
+      return true;
+    })
+    .catch((resp) => errorCB(resp));
+};
 
-export const requestJson = (port, topic, json2send, jsonReceivedCB, errorCB) => {
-	ipcRenderer.invoke("request", {
-		data: {
-			json2send
-		},
-		method: "POST",
-		url: "http://127.0.0.1:" + port + "/" + topic,
-	},
-	).then((data) => {
-		jsonReceivedCB(data["data"])
-		return true
-	}).catch((resp) => errorCB(resp));
-}
-
-
+export const axiosPostJson = async (jsonData) => {
+  try {
+    const response = await axios.post(
+      "http://localhost:5000/api/message",
+      jsonData
+    );
+    return response.data.response;
+  } catch (error) {
+    return error;
+  }
+};
 
 // example of use:
-{/*
+{
+  /*
 
 import { requestJson } from '/utilities/requests';
 
@@ -32,41 +51,38 @@ import { requestJson } from '/utilities/requests';
     }
 }>send test</Button> 
 
-*/}
-
+*/
+}
 
 export const downloadFile = (downloadUrl, onSuccess, onError) => {
-	console.log("DownloadFile: " + downloadUrl);
-	if (downloadUrl) {
-		var xhr = new XMLHttpRequest();
-		xhr.open("GET", downloadUrl);
-		xhr.onload = function () {
-			if (xhr.status == 200) {
-				onSuccess(xhr.responseText);
-			}
-			else {
-				onError(xhr.status + " " + xhr.statusText);
-			}
-		};
-		xhr.onerror = function (e) {
-			console.log(e);
-			onError(e);
-		};
-		xhr.send();
-	}
-}
+  console.log("DownloadFile: " + downloadUrl);
+  if (downloadUrl) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", downloadUrl);
+    xhr.onload = function () {
+      if (xhr.status == 200) {
+        onSuccess(xhr.responseText);
+      } else {
+        onError(xhr.status + " " + xhr.statusText);
+      }
+    };
+    xhr.onerror = function (e) {
+      console.log(e);
+      onError(e);
+    };
+    xhr.send();
+  }
+};
 
-export const getQueryParams = () =>{
-	var a = window.location.search.substr(1);
-	if (a == "") return {};
-	var params = a.split("&");
-	var b = {};
-	for (var i = 0; i < params.length; ++i) {
-		var p = params[i].split("=", 2);
-		if (p.length == 1)
-			b[p[0]] = "";
-		else
-			b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
-	}
-	return b;
-}
+export const getQueryParams = () => {
+  var a = window.location.search.substr(1);
+  if (a == "") return {};
+  var params = a.split("&");
+  var b = {};
+  for (var i = 0; i < params.length; ++i) {
+    var p = params[i].split("=", 2);
+    if (p.length == 1) b[p[0]] = "";
+    else b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+  }
+  return b;
+};
