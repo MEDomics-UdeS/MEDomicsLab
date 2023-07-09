76f74e1b4200e3df5af3b80362aad23c3b867f3f
AndreanneAllaire <andreanne.allaire@usherbrooke.ca>
2023-07-09 19:58:10 -0400

    Added folder Flask_server/MEDimageApp for the backend of extraction tab. Added extraction tab as a blueprint in the flask app. Added .gitmodules for MEDimage module.

---
+++ Flask_server/MEDimageApp/utils.py
@@ -0,0 +1,102 @@
+import numpy as np
+from MEDimageApp.figure import Figure
+
+
+# Remplace la fonction allowed_file de app_extraction_blueprint.py
+def allowed_pickle_object(filepath):
+    extension = '.npy'
+    # TODO : Plus de paramètres que l'extension doivent être vérifiés pour le pickle object
+    return filepath.endswith(extension)
+
+
+# TODO : Pour remplacer la fonction formatFeatures de app_extraction_blueprint.py
+def format_features(features_dict):
+    return {k: (np.float64(v) if not isinstance(v, list) else v) for (k, v) in features_dict.items()}
+
+
+####################UTILS FOR NODES#########################################
+# Remplace la fonction getNodeContentFromId de app_extraction_blueprint.py
+# TODO : REFACTOR
+def get_node_content(id: str, json_scene):
+    find = False
+    for module in json_scene['drawflow']:  # We scan all module in scene
+        for node_id in json_scene['drawflow'][module]['data']:  # We scan all node of each modulef in scene
+            if (node_id == str(id)):  # they are "str"
+                find = True
+                content = json_scene['drawflow'][module]['data'][node_id]
+
+    if (find == True):
+        return content
+    else:
+        print("No node with this ID")
+        return 0
+
+
+# Find all instances of key value in dictionnary : https://stackoverflow.com/questions/9807634/find-all-occurrences-of-a-key-in-nested-dictionaries-and-lists
+def gen_dict_extract(key, var):
+    if hasattr(var, 'items'):
+        for k, v in var.items():
+            if k == key:
+                yield v
+            if isinstance(v, dict):
+                for result in gen_dict_extract(key, v):
+                    yield result
+            elif isinstance(v, list):
+                for d in v:
+                    for result in gen_dict_extract(key, d):
+                        yield result
+
+
+# Instantiates image figure when calling view on node from app_extraction_blueprint.py
+def image_viewer(medimage_list, data, runs):
+    # Check if the node to view is input
+    if data["name"] == "input":
+        fig_title = "3D Volume of \"" + data["file_loaded"] + "\""
+
+        vol = medimage_list[data["file_loaded"]].data.volume.array
+
+        fig = Figure(vol, fig_title)
+        fig.add_data()
+        fig.create_figure_sliders()
+        fig.update_figure_layout()
+        fig.show_figure()
+
+    # If node is not input, need to retrace pipeline
+    else:
+        mask = None
+        last_run = list(runs)[-1]
+        # 3D view created for each pip related to the view button clicked
+        for pip in runs[last_run]:
+            for id in runs[last_run][pip]:
+
+                if ("node-" + str(id) == str(data["id"])):
+                    if (data["name"] == "re_segmentation" or data["name"] == "segmentation"):
+                        # Code cleaning for ROI contour tracing
+                        mask = runs[last_run][pip][id]['output']['roi']
+                        if type(mask) != np.ndarray:
+                            mask = mask.data
+
+                        vol = runs[last_run][pip][id]['output']['vol']
+                        if type(vol) != np.ndarray:
+                            vol = vol.data
+
+                    else:
+                        vol = runs[last_run][pip][id]['output']['vol']  # display VOL for others nodes
+                    print(type(runs[last_run][pip][id]['output']['vol']))
+                    if type(vol) != np.ndarray:
+                        vol = vol.data
+
+                    # Figure title
+                    fig_title = ""
+                    if (runs[last_run][pip][id]["type"] == "filter"):
+                        fig_title = '3D Volume - Pipeline ' + pip + " - " + runs[last_run][pip][id]["settings"][
+                            "filter_type"] + \
+                                    " " + runs[last_run][pip][id]["type"] + " output."
+                    else:
+                        fig_title = '3D Volume - Pipeline ' + pip + " - " + runs[last_run][pip][id]["type"] + " output."
+
+                    fig = Figure(vol, fig_title, mask)
+                    fig.add_data()
+                    fig.create_figure_sliders()
+                    fig.update_figure_layout()
+                    fig.show_figure()
--- Flask_server/server.py
+++ Flask_server/server.py
@@ -6,13 +6,17 @@ from utils.server_utils import get_json_from_request
 import json
 
 # Import blueprint for extraction tab
+from app_extraction_blueprint import app_extraction
 
 # Creating main instance of Flask app
 app = Flask(__name__)
 
-# Configure Flask-CORS to specify tha only requests coming from local port 8888 are allowed 
-# to access the API route /*
+# Configure Flask-CORS to specify that only requests coming from local port 8888 are allowed 
+# to access the API route 
 CORS(app, resources={r"/*": {"origins": "http://localhost:8888"}})
+
+# Register blueprint for extraction tab
+app.register_blueprint(app_extraction, url_prefix='/extraction')
 
 @app.route('/test', methods=['GET', 'POST'])
 def test():
@@ -29,12 +33,12 @@ def run_experiment():
     return run_experiment(data)
 
 # Test server to receive message
-@app.route('/message', methods=['POST'])
+@app.route("/message", methods=["POST"])
 def receive_message():
     data = request.json
-    message = data['message']
+    message = data["message"]
     # Process the message or perform any necessary logic
-    response = {'response': f'Received message: {message}'}
+    response = {"response": f"Received message: {message}"}
     return jsonify(response)
 
 
