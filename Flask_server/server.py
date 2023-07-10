23a854081c2730dfc3370e5f3ada73ddbd6a8bb3
AndreanneAllaire <andreanne.allaire@usherbrooke.ca>
2023-07-09 20:41:17 -0400

    Fixed flask server following merge with develop_Guillaume.

--- Flask_server/server.py
+++ Flask_server/server.py
@@ -1,24 +1,23 @@
-from learning.app_learning_blueprint import app_learning
-import sys
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, Blueprint
-from flask_cors import CORS
-from learning.learning_processing import run_experiment
-from utils.server_utils import get_json_from_request
 import json
+from flask import Flask, request, jsonify, Blueprint
+from flask_cors import CORS
+from utils.server_utils import get_json_from_request
 
-# Import blueprint for extraction tab
+# Import blueprints
 from app_extraction_blueprint import app_extraction
+from learning.app_learning_blueprint import app_learning
 
 # Creating main instance of Flask app
 app = Flask(__name__)
-app.register_blueprint(app_learning, url_prefix='/learning')
+
 
 # Configure Flask-CORS to specify that only requests coming from local port 8888 are allowed 
 # to access the API route 
 CORS(app, resources={r"/*": {"origins": "http://localhost:8888"}})
 
-# Register blueprint for extraction tab
+# Register blueprints
 app.register_blueprint(app_extraction, url_prefix='/extraction')
+app.register_blueprint(app_learning, url_prefix='/learning')
 
 @app.route('/test', methods=['GET', 'POST'])
 def test():
