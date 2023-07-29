fd2a37e25cd9b42dfefa5e2ad541c324b81a6ce0
AndreanneAllaire <andreanne.allaire@usherbrooke.ca>
2023-07-29 14:42:48 -0400

    Added body to /flow/groupNode.jsx to be able to add a run button in the extractionNode of extraction tab.

--- renderer/components/flow/groupNode.jsx
+++ renderer/components/flow/groupNode.jsx
@@ -1,48 +1,66 @@
 import React, { useContext } from "react"
-import { Button } from "react-bootstrap";
+import { Button } from "react-bootstrap"
 import Card from "react-bootstrap/Card"
-import CloseButton from "react-bootstrap/CloseButton";
+import CloseButton from "react-bootstrap/CloseButton"
 import Handlers from "./handlers"
-import { FlowInfosContext} from "./context/flowInfosContext";
-
+import { FlowInfosContext } from "./context/flowInfosContext"
 
 /**
- * 
+ *
  * @param {*} id id of the node
  * @param {*} data data of the node
  * @param {*} type type of the node
  * @returns {JSX.Element} A GroupNode
- * 
+ *
  * @description
  * This component is used to display a GroupNode.
  * A GroupNode is a node that contains a subflow, so it handles a click that change the active display subflow.
- * It does not implement a Node because it does not need to have access to an offcanvas 
+ * It does not implement a Node because it does not need to have access to an offcanvas
  */
-const GroupNode = ({ id, data }) => {
-	const { flowInfos } = useContext(FlowInfosContext);		// used to get the flow infos
+const GroupNode = ({ id, data, nodeBody }) => {
+  const { flowInfos } = useContext(FlowInfosContext) // used to get the flow infos
 
-	return (
-		<>
-			<div>
-				<div >
-					<Handlers id={id} setupParam={data.setupParam} tooltipBy={data.tooltipBy}/>
-					<Card key={`${id}`} id={`${id}`} className="text-left node">
-						<Card.Header onClick={() => data.parentFct.changeSubFlow(id)}>
-							<img src={"/icon/"+flowInfos.type+"/" + `${data.internal.img.replaceAll(" ", "_")}`} alt={data.internal.img} className="icon-nodes" />
-							{data.internal.name}
-						</Card.Header>
-					</Card>
-				</div>
+  return (
+    <>
+      <div>
+        <div>
+          <Handlers
+            id={id}
+            setupParam={data.setupParam}
+            tooltipBy={data.tooltipBy}
+          />
+          <Card key={`${id}`} id={`${id}`} className="text-left node">
+            <Card.Header onClick={() => data.parentFct.changeSubFlow(id)}>
+              <img
+                src={
+                  "/icon/" +
+                  flowInfos.type +
+                  "/" +
+                  `${data.internal.img.replaceAll(" ", "_")}`
+                }
+                alt={data.internal.img}
+                className="icon-nodes"
+              />
+              {data.internal.name}
+            </Card.Header>
+            {/* body of the node*/}
+            {nodeBody != undefined && <Card.Body>{nodeBody}</Card.Body>}
+          </Card>
+        </div>
 
-				<CloseButton onClick={() => data.parentFct.deleteNode(id)} />
-				{data.setupParam.classes.split(" ").includes("run") &&
-					<Button variant="success" className='btn-runNode' onClick={() => data.parentFct.runNode(id)}>
-						<img src={"/icon/run.svg"} alt="run" className="img-fluid" />
-					</Button>
-				}
-			</div >
-		</>
-	)
+        <CloseButton onClick={() => data.parentFct.deleteNode(id)} />
+        {data.setupParam.classes.split(" ").includes("run") && (
+          <Button
+            variant="success"
+            className="btn-runNode"
+            onClick={() => data.parentFct.runNode(id)}
+          >
+            <img src={"/icon/run.svg"} alt="run" className="img-fluid" />
+          </Button>
+        )}
+      </div>
+    </>
+  )
 }
 
 export default GroupNode
