7f7a3813c4fd15d4b1453befca2114e4e6324267
NicoLongfield <72104945+NicoLongfield@users.noreply.github.com>
2023-09-07 11:25:41 -0400

    Test de push sur un PR

--- main/background.js
+++ main/background.js
@@ -88,34 +88,6 @@ if (isProd) {
 	]
 
 
-	// ca fonctionne pas pour l'instant
-	// // var conda = require('child_process').spawn('C:\\Users\\gblai\\anaconda3\\Scripts\\conda.exe', ['run', '-n', 'med', '/bin/bash', '-c', 'source activate med']);
-	// // var conda = require('child_process').spawn('C:\\Users\\gblai\\anaconda3\\Scripts\\conda.exe', ['activate', 'med']);
-	// var conda = require('child_process').spawn('cmd.exe', ['/c', 'call', '/v', '/k', path.join("C:\\Users\\gblai\\anaconda3\\envs\\med\\Lib\\site-packages\\virtualenv\\activation\\batch\\activate.bat"), 'med']);
-
-	// conda.on('exit', function (code) {
-	//   if (code === 0) {
-	//     console.error('Failed to activate conda environment');
-	//     return;
-	//   }
-
-	//   // execute the Python file
-	//   var python = require('child_process').spawn('py', ['./Flask_server/server.py']);
-
-	//   python.stdout.on('data', function (data) {
-	//     console.log(data.toString());
-	//   });
-
-	//   python.stderr.on('data', function (data) {
-	//     console.error(data.toString());
-	//   });
-	//   python.on('exit', function (code) {
-	//     console.log('Child process exited with code ' + code);
-	//   });
-	// });
-	// conda.catch(function (err) {
-	//   console.error(err);
-	// });
 
 	ipcMain.handle("request", async (_, axios_request) => {
 		const result = await axios(axios_request)
