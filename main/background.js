93656892a4e845d97cf362032c93f6558eb4301e
NicoLongfield <72104945+NicoLongfield@users.noreply.github.com>
2023-09-07 11:48:50 -0400

    Commented the background.js

--- main/background.js
+++ main/background.js
@@ -94,17 +94,17 @@ if (isProd) {
 		return { data: result.data, status: result.status }
 	})
 
-	ipcMain.on("messageFromNext", (event, data) => {
+	ipcMain.on("messageFromNext", (event, data) => { // Receives a message from Next.js
 		console.log("messageFromNext : ", data);
-		if (data === "requestDialogFolder") {
+		if (data === "requestDialogFolder") { // If the message is "requestDialogFolder", the function setWorkingDirectory is called
 			setWorkingDirectory(event, mainWindow);
 		}
-		else if (data === "requestWorkingDirectory") {
+		else if (data === "requestWorkingDirectory") { // If the message is "requestWorkingDirectory", the function getTheWorkingDirectoryStructure is called and the folder structure is returned to Next.js
 			event.reply("messageFromElectron", { "workingDirectory": dirTree(app.getPath('sessionData')), "hasBeenSet": hasBeenSet });
 			event.reply("workingDirectorySet", { "workingDirectory": dirTree(app.getPath('sessionData')), "hasBeenSet": hasBeenSet });
 		}
 		else if (data === "updateWorkingDirectory") {
-			event.reply("updateDirectory", { "workingDirectory": dirTree(app.getPath('sessionData')), "hasBeenSet": hasBeenSet });
+			event.reply("updateDirectory", { "workingDirectory": dirTree(app.getPath('sessionData')), "hasBeenSet": hasBeenSet }); // Sends the folder structure to Next.js
 		}
 		else if (data === "requestAppExit") {
 			app.exit();
@@ -124,34 +124,40 @@ if (isProd) {
 	}
 })();
 
-
+/**
+ * @description Set the working directory
+ * @summary Opens the dialog to select the working directory and  creates the folder structure if it does not exist
+ *          When the working directory is set, the function returns the folder structure of the working directory as a JSON object in a reply to Next.js
+ * @param {*} event 
+ * @param {*} mainWindow 
+ */
 function setWorkingDirectory(event, mainWindow) {
-	dialog.showOpenDialog(mainWindow, {
+	dialog.showOpenDialog(mainWindow, { // Opens the dialog to select the working directory (Select a folder window)
 		properties: ['openDirectory']
 	}).then(result => {
-		if (result.canceled) {
+		if (result.canceled) { // If the user cancels the dialog
 			console.log('Dialog was canceled')
 			event.reply("messageFromElectron", "Dialog was canceled");
 		} else {
 			const file = result.filePaths[0]
 			console.log(file)
-			if (file === app.getPath('sessionData')) {
-				console.log('Working directory is already set to ' + file)
+			if (file === app.getPath('sessionData')) { // If the working directory is already set to the selected folder
+				console.log('Working directory is already set to ' + file) 
 				event.reply("messageFromElectron", 'Working directory is already set to ' + file);
-				event.reply("workingDirectorySet", { "workingDirectory": dirTree(file), "hasBeenSet": hasBeenSet });
+				event.reply("workingDirectorySet", { "workingDirectory": dirTree(file), "hasBeenSet": hasBeenSet }); 
 
 			}
-			else {
+			else { // If the working directory is not set to the selected folder
+				// The working directory is set to the selected folder and the folder structure is returned to Next.js
 				console.log('Working directory set to ' + file)
 				event.reply("messageFromElectron", 'Working directory set to ' + file);
 				app.setPath('sessionData', file);
 				createWorkingDirectory();
-				hasBeenSet = true;
+				hasBeenSet = true; // The boolean hasBeenSet is set to true to indicate that the working directory has been set
+				// This is the variable that controls the disabled/enabled state of the IconSidebar's buttons in Next.js
 				event.reply("messageFromElectron", dirTree(file));
 				event.reply("workingDirectorySet", { "workingDirectory": dirTree(file), "hasBeenSet": hasBeenSet });
 
-				//   app.relaunch({ e})
-				//   mainWindow.loadURL(`file://${file}`)
 			}
 		}
 	}).catch(err => {
@@ -160,7 +166,7 @@ function setWorkingDirectory(event, mainWindow) {
 }
 
 
-function createWorkingDirectory() {
+function createWorkingDirectory() { // See the workspace template in the repository
 	createFolder("DATA");
 	createFolder("EXPERIMENTS");
 	createFolder("MODELS");
@@ -168,7 +174,7 @@ function createWorkingDirectory() {
 }
 
 
-function createFolder(folderString) {
+function createFolder(folderString) { // Creates a folder in the working directory
 	const folderPath = path.join(app.getPath('sessionData'), folderString);
 
 	fs.mkdir(folderPath, { recursive: true }, (err) => {
@@ -182,21 +188,16 @@ function createFolder(folderString) {
 }
 
 
-function getTheWorkingDirectoryStructure() {
+function getTheWorkingDirectoryStructure() { // Returns the folder structure of the working directory
 	const dirTree = require("directory-tree");
 	const tree = dirTree(getWorkingDirectory());
 	return tree;
 }
 
-function getWorkingDirectory() {
+function getWorkingDirectory() { // Returns the working directory
 	return app.getPath('sessionData');
 }
 
-
-// ipcMain.handle("request", async (_, axios_request) => {
-// 	const result = await axios(axios_request)
-// 	return { data: result.data, status: result.status }
-// })
 
 app.on("window-all-closed", () => {
 	app.quit();
