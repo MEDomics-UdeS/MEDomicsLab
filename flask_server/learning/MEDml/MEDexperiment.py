e961a7d925629df752fb9e94013fdf68e5d8265e
Blain354 <gblain03@hotmail.com>
2023-09-11 16:02:53 -0400

    correction path MEDexperiment

--- flask_server/learning/MEDml/MEDexperiment.py
+++ flask_server/learning/MEDml/MEDexperiment.py
@@ -68,7 +68,7 @@ class MEDexperiment:
         self._nb_nodes_done: float = 0.0
         self.global_json_config['unique_id'] = 0
         self.pipelines_objects = self.create_next_nodes(self.pipelines, copy.deepcopy(self.pipelines_objects))
-        global_json_config['saving_path'] = "Flask_server/local_dir"
+        global_json_config['saving_path'] = "flask_server/local_dir"
         tmp_dir = global_json_config['saving_path']
         for f in os.listdir(tmp_dir):
             if f != '.gitkeep':
