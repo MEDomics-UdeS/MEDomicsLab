13bd5567181de100d1171ee4a2fd9c159560a47a
NicoLongfield <lonn1001@usherbrooke.ca>
2023-09-20 15:58:40 -0400

    file creation

---
+++ renderer/components/mainPages/dataComponents/datasetSelector.jsx
@@ -0,0 +1,44 @@
+import React, { useContext, useState, useEffect } from "react"
+import { DataContext } from "../../workspace/dataContext"
+
+/**
+ * @description - This component is the dataset selector component that will show the datasets available in the workspace
+ * @returns the dataset selector component
+ * @param {Object} props - The props object
+ *  @param {Object} props.keepOnlyFolder - The only parent folder to keep in the dataset selector
+ */
+const DatasetSelector = (props) => {
+  const { globalData, setGlobalData } = useContext(DataContext) // We get the global data from the context to retrieve the directory tree of the workspace, thus retrieving the data files
+  const [datasetList, setDatasetList] = useState([])
+  const [selectedDatasets, setSelectedDatasets] = useState("")
+
+  function generateDatasetListFromDataContext(dataContext) {
+    let datasetList = []
+    let keys = Object.keys(dataContext)
+    keys.forEach((key) => {
+      if (dataContext[key].type === "folder") {
+        datasetList.push(dataContext[key].name)
+      }
+    })
+
+  useEffect(() => {
+    if (globalData !== undefined) {
+      
+      generateDatasetListFromDataContext(globalData)
+    }
+  }, [globalData])
+      
+
+  function handleDatasetSelect(event) {
+    console.log("Dataset selected", event.target.value)
+    setGlobalData({ ...globalData, dataset: event.target.value })
+  }
+
+  return (
+    <>
+      <h1>Extraction Time Series Page - TO BE IMPLEMENTED</h1>
+    </>
+  )
+}
+
+export default DatasetSelector
--- renderer/components/mainPages/input.jsx
+++ renderer/components/mainPages/input.jsx
@@ -1,29 +1,20 @@
-import React from "react";
-import DropzoneComponent from "./dataComponents/dropzoneComponent";
-// import Datatable from "./dataComponents/datatable"; // Imports are commented for the moment to avoid errors
-// import SheetJSTable from "./dataComponents/sheetjsTest";
-import { Button } from "react-bootstrap";
+import React from "react"
+import DropzoneComponent from "./dataComponents/dropzoneComponent"
+import { Button } from "react-bootstrap"
+import { DatasetSelector } from "./dataComponents/datasetSelector"
 
 /**
  * @description - This component is the input page of the application
  * @returns the input page component
  */
 const InputPage = () => {
-	// eslint-disable-next-line no-unused-vars
-	const [data, setData] = React.useState([]);
-	return (
-		<>
-			<h1>INPUT MODULE</h1>
-			<DropzoneComponent setData={setData}>
-				<Button style={{alignItems: "flex-end",  marginInline:"2%"}}/>	
-			</DropzoneComponent>
-			<div style={{ display:"flex", overflow:"scroll"}}>
-				{/* <Datatable /> */} 
-				{/* <SheetJSTable /> */}
-			</div>
-		</>
-	);
-};
+  // eslint-disable-next-line no-unused-vars
+  const [data, setData] = React.useState([])
+  return (
+    <>
+      <h1>INPUT MODULE</h1>
+    </>
+  )
+}
 
-
-export default InputPage;
+export default InputPage
