4f309229c3562bff0ee0fe339fb4cb5f8664f7dd
NicoLongfield <lonn1001@usherbrooke.ca>
2023-10-11 14:36:53 -0400

    Splash screen working

--- flask_server/server.py
+++ flask_server/server.py
@@ -1,6 +1,7 @@
 from learning.app_learning_blueprint import app_learning
 from extraction_ts.app_extraction_ts_blueprint import app_extraction_ts
 from extraction.app_extraction_blueprint import app_extraction
+from input.app_input_blueprint import app_input
 import json
 import argparse
 from flask import Flask, request, jsonify
@@ -20,6 +21,7 @@ app = Flask(__name__)
 app.register_blueprint(app_extraction, url_prefix='/extraction')
 app.register_blueprint(app_learning, url_prefix='/learning')
 app.register_blueprint(app_extraction_ts, url_prefix='/extraction_ts')
+app.register_blueprint(app_input, url_prefix='/input')
 
 
 @app.route('/test', methods=['GET', 'POST'])
