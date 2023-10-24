8880a461868a547bd64a38c77d36d8372176f2af
Blain354 <gblain03@hotmail.com>
2023-10-24 13:26:24 -0400

    asynchone image showing

--- renderer/components/learning/results/node/analyseResults.jsx
+++ renderer/components/learning/results/node/analyseResults.jsx
@@ -4,9 +4,11 @@ import MedDataObject from "../../../workspace/medDataObject"
 import { modifyZipFileSync } from "../../../../utilities/customZipFile"
 
 const getLocalImage = (src) => {
-  const nativeImage = require("electron").nativeImage
-  const image = nativeImage.createFromPath(src)
-  return image.toDataURL()
+  return new Promise((resolve, reject) => {
+    const nativeImage = require("electron").nativeImage
+    const image = nativeImage.createFromPath(src)
+    resolve(image.toDataURL())
+  })
 }
 
 /**
@@ -16,38 +18,51 @@ const getLocalImage = (src) => {
  */
 const AnalyseResults = ({ selectedResults }) => {
   const [images, setImages] = useState([])
-  const [addedKeys, setAddedKeys] = useState([])
 
-  /**
-   *
-   * @param {String} modelName The name of the model
-   * @param {String} path The path of the image
-   */
-  const createImageFromZip = async (modelName, path) => {
-    let zipPath = path.split(MedDataObject.getPathSeparator() + "tmp" + MedDataObject.getPathSeparator())[0]
-    await modifyZipFileSync(zipPath + ".medml", async () => {
-      !addedKeys.includes(modelName) &&
-        images.push(
-          <div key={modelName}>
-            <Image src={getLocalImage(path)} alt="Image" height="250" indicatorIcon={<h5>{modelName}</h5>} preview downloadable />
-          </div>
-        ),
-        setImages(images)
-      addedKeys.push(modelName)
-      setAddedKeys(...addedKeys)
-    })
-  }
-
-  // Create the images from the zip files when the selected results change
   useEffect(() => {
-    if (selectedResults.data) {
-      Object.entries(selectedResults.data).forEach(async ([modelName, path]) => {
-        if (!addedKeys.includes(modelName)) {
-          await createImageFromZip(modelName, path)
-        }
+    if(selectedResults.data){
+      let imagesInfos = []
+      Object.entries(selectedResults.data).forEach(([modelName, path]) => {
+        console.log("path", path)
+        imagesInfos.push({ modelName: modelName, path: path })
       })
-    }
-  }, [selectedResults.data, addedKeys])
+      createImages(imagesInfos)
+    } 
+  }, [selectedResults])
+
+
+  const createImages = (imagesInfos) => {
+    if(imagesInfos.length != 0){
+      console.log("imagesInfos", imagesInfos)
+      let zipPath = imagesInfos[0].path.split(MedDataObject.getPathSeparator() + "tmp" + MedDataObject.getPathSeparator())[0]
+      let images = []
+      console.log("zipPath", zipPath)
+      modifyZipFileSync(zipPath + ".medml", () => {
+        const addImage = (modelName, path) => {
+          return new Promise(resolve => {
+            getLocalImage(path).then((imageUrl) => {
+              images.push(
+                <div key={modelName}>
+                  <Image src={imageUrl} alt="Image" height="250" indicatorIcon={<h5>{modelName}</h5>} preview downloadable />
+                </div>)
+              resolve();
+            })
+        })
+        }
+        return new Promise(resolve => {
+          let chain = Promise.resolve();
+          for (let {modelName, path} of imagesInfos) {
+            console.log("path", path)
+            chain = chain.then(()=>addImage(modelName, path))
+          }
+          resolve()
+          })
+        }).then(() => {
+          setImages(images)
+        }
+      )
+    } 
+  }
 
   return (
     <div className="height-100 width-100 flex-grid-gap-1rem">
