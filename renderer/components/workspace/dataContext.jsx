576a8667eb5b60eade3fc6a4d350cdf9da257f03
Blain354 <gblain03@hotmail.com>
2023-10-25 09:36:11 -0400

    added doc for dataContext.jsx

--- renderer/components/workspace/dataContext.jsx
+++ renderer/components/workspace/dataContext.jsx
@@ -14,11 +14,18 @@ const DataContext = createContext(null)
  * @param {Object} props.children - The children components to wrap with the DataContext context object.
  * @param {Object} props.globalData - The global data object to provide to the children components.
  * @param {Function} props.setGlobalData - The function to update the global data object.
+ * @param {Object} props.dataRequest - The data request object to provide to the children components.
+ * @param {Function} props.setDataRequest - The function to update the data request object.
+ * @param {Function} props.copyGlobalDataSync - The function to copy the global data object.
  * @returns {JSX.Element} - The DataContextProvider component.
  */
 function DataContextProvider({ children, globalData, setGlobalData }) {
-  const [dataRequest, setDataRequest] = useState({})
+  const [dataRequest, setDataRequest] = useState({}) // The data request object that will be used to request data from the main process
 
+  /**
+   * 
+   * @returns {Promise} - A promise that resolves with a copy of the global data object.
+   */
   const copyGlobalDataSync = () => {
     return new Promise((resolve) => {
       let globalDataCopy = { ...globalData }
