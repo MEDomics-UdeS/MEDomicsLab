cafe2e13278be6658364326786560de7c64e8cb9
Sari27 <sarah.dns.sd@gmail.com>
2023-11-16 10:13:24 -0500

    moved MEDprofiles css under input

--- renderer/pages/_app.js
+++ renderer/pages/_app.js
@@ -53,7 +53,7 @@ import "../styles/evaluation/evaluation.css"
 import "../styles/output.css"
 import "../styles/exploratory/exploratory.css"
 import "../styles/application/application.css"
-import "../styles/medProfiles.css"
+
 /**
  * This is the main app component. It is the root component of the app.
  * It is the parent of all other components.
