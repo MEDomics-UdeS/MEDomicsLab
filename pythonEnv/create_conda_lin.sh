8ec125d2d37591e2429a896841ed69b6597cd5e3
Blain354 <gblain03@hotmail.com>
2023-11-23 11:33:06 -0500

    linux build working

---
+++ pythonEnv/create_conda_lin.sh
@@ -0,0 +1,6 @@
+eval "$(conda shell.bash hook)"
+
+# Activate the conda environment
+conda activate med
+
+# Rest of your script goes here
--- utilScripts/pack_GO.sh
+++ utilScripts/pack_GO.sh
@@ -2,3 +2,4 @@ cd go_server
 go build main.go
 cd ..
 echo F | cp go_server/main renderer/public/server_go
+echo F | cp go_server/main resources/server_go
