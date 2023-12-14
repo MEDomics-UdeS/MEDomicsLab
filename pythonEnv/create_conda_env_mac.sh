5b98b339b0efe3e48e449fdd2e29564c37047f60
NicoLongfield <72104945+NicoLongfield@users.noreply.github.com>
2023-12-14 16:36:50 -0500

    Dev med profile viewer merging (#180)
    * Fixing the installation of the python environment
    
    * Progress, fixed the import in predict.py
    
    * Input, Exploratory, Learning and Application Module working, need to investigate XGBoost (with application)
    
    * Mac working
    
    * Mac working
    
    * Merged and changed automatic building with conda env with .yml
    
    * Fixed .yml
    
    * Changes for Linux building
    
    * Fix icon not showing on linux
    
    * Changed the go version to 1.19
    
    * Merging in MEDProfilesFigure working
    
    * Merge working with duplicate column name handling implemented
    
    * Deleted local_dir files
    
    * Removed test.py and test_biobert.py from pythonCode
    
    * Cleaning the PR
    
    * Modified ubuntu version for Github Actions
    
    * PR cleaning
    
    * Cleaning the prints in Biobert
    
    * Cleaning the prints in Biobert
    
    * Fix error of unique key - maybe...
    
    * Added submodules for automatic building

--- pythonEnv/create_conda_env_mac.sh
+++ pythonEnv/create_conda_env_mac.sh
@@ -61,7 +61,7 @@ if conda env list | grep -q 'med_conda_env'; then
 else
     # Create a new environment
     echo "Creating a new environment..."
-    conda create -n med_conda_env python=3.9 -y || {
+    conda create -n med_conda_env python==3.9.18 -y || {
         echo "An error occurred while creating the new environment."
         exit 1
     }
@@ -74,12 +74,79 @@ conda activate med_conda_env || {
     exit 1
 }
 
-# Install packages from requirements.txt
-echo "Installing packages from requirements.txt..."
-pip install -r "$(dirname "$0")/requirements.txt" || {
-    echo "An error occurred while installing packages from requirements.txt."
+# Install xcode command line tools
+echo "Installing xcode command line tools..."
+xcode-select --install || {
+    echo "An error occurred while installing xcode command line tools."
+}
+
+# Check if homebrew is installed
+if command -v brew &> /dev/null; then
+    echo "Homebrew is already installed."
+else
+    echo "Homebrew is not installed."
+    # Install homebrew
+    echo "Installing homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || {
+    echo "An error occurred while installing homebrew."
+    exit 1
+    }
+    # Add homebrew to PATH in .zprofile and .bash_profile
+    echo "Adding homebrew to PATH..."
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/med/.zprofile || {
+        echo "An error occurred while adding homebrew to PATH."
+        exit 1
+    }
+    eval "$(/opt/homebrew/bin/brew shellenv)" || {
+        echo "An error occurred while adding homebrew to PATH."
+        exit 1
+    }
+fi
+
+# Install libomp
+echo "Installing libomp..."
+brew install libomp || {
+    echo "An error occurred while installing libomp."
     exit 1
 }
+
+# Install miniforge
+echo "Installing miniforge..."
+brew install miniforge || {
+    echo "An error occurred while installing miniforge."
+    exit 1
+}
+
+# Install pycaret without dependencies
+echo "Installing pycaret without dependencies..."
+pip install pycaret --no-dependencies || {
+    echo "An error occurred while installing pycaret without dependencies."
+    exit 1
+}
+
+# Install packages from mac_requirements.txt
+echo "Installing packages from mac_requirements.txt..."
+cat mac_requirements.txt | xargs -n 1 conda install -y || {
+    echo "An error occurred while installing packages from mac_requirements.txt."
+}
+
+conda install -c conda-forge category_encoders -y || {
+    echo "An error occurred while installing category_encoders."
+    exit 1
+}
+
+conda install -c conda-forge python-xxhash -y || {
+    echo "An error occurred while installing python-xxhash."
+    exit 1
+}
+
+pip install xgboost --no-binary xgboost -v || {
+    echo "An error occurred while installing xgboost."
+    exit 1
+}
+
+
+
 
 # Deactivate the new environment
 echo "Deactivating the new environment..."
