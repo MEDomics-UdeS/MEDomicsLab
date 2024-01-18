5537dfddbef5fe2981673fc2d452037acdef2272
NicoLongfield <lonn1001@usherbrooke.ca>
2024-01-18 17:33:36 -0500

    Merge with tags correction

--- pythonCode/modules/input/merge.py
+++ pythonCode/modules/input/merge.py
@@ -1,10 +1,11 @@
 import json
 import sys
 import os
+import collections
 from pathlib import Path
 sys.path.append(
     str(Path(os.path.dirname(os.path.abspath(__file__))).parent.parent))
-from med_libs.input_utils.dataframe_utilities import load_data_file, save_dataframe
+from med_libs.input_utils.dataframe_utilities import load_data_file, save_dataframe, handle_tags_in_dataframe
 from med_libs.GoExecutionScript import GoExecutionScript, parse_arguments
 from med_libs.server_utils import go_print
 
@@ -41,7 +42,7 @@ class GoExecScriptMerge(GoExecutionScript):
         length = (len(payload.keys())-1)*2
         progress_step = 100/length
         progress = 0
-
+        tags = {}
         
 
         self.set_progress(now=progress, label="Initialisation")
@@ -52,9 +53,10 @@ class GoExecScriptMerge(GoExecutionScript):
         first_dataset_selected_columns = [
             column for column in first_dataset_selected_columns if column != None]
         self.set_progress(now=progress, label="Initialisation : Loading the first dataset")
-        first_dataset = load_data_file(first_dataset_path, first_dataset_extension)[
-            first_dataset_selected_columns]
-
+        first_dataset = load_data_file(first_dataset_path, first_dataset_extension)
+        first_dataset, first_dataset_tags = handle_tags_in_dataframe(first_dataset)
+        first_dataset = first_dataset[first_dataset_selected_columns]
+        tags.update(first_dataset_tags)
         for dataset in payload.keys():
             if (dataset == "0"):
                 continue
@@ -66,24 +68,52 @@ class GoExecScriptMerge(GoExecutionScript):
                 dataset_merge_on = payload[dataset]["mergeOn"]
                 dataset_merge_type = payload[dataset]["mergeType"]
 
+                # Dataset name without extension
+                dataset_name_without_ext = dataset_name.split(".")
+                if len(dataset_name_without_ext) > 2:
+                    dataset_name_without_ext = dataset_name_without_ext[:-1]
+                    dataset_name_without_ext = ".".join(dataset_name_without_ext)
+                else:
+                    dataset_name_without_ext = dataset_name_without_ext[0]
+
                 # Update the progress
                 self.set_progress(now=progress, label="Initialisation : Loading the first dataset")
                 progress += progress_step
 
                 # Load the dataset
-                new_dataframe = load_data_file(dataset_path, dataset_extension)[
-                    dataset_selected_columns]
+                new_dataframe = load_data_file(dataset_path, dataset_extension)
+                new_dataframe, new_dataframe_tags = handle_tags_in_dataframe(new_dataframe)
+                new_dataframe = new_dataframe[dataset_selected_columns]
                 self.set_progress(now=progress, label="Merging with " + dataset_name)
                 progress += progress_step
 
+                # Check if the new dataframe has tags
+                if len(new_dataframe_tags) > 0:
+                    # Check if the column names of the new dataframe are already present in the first dataset
+                    first_dataset_columns = first_dataset.columns
+                    intersection = collections.Counter(first_dataset_columns) & collections.Counter(new_dataframe.columns)
+                    if len(intersection) > 0:
+                        # Remove the merge on column from the intersection
+                        if dataset_merge_on in intersection.keys():
+                            intersection.pop(dataset_merge_on)
+                        # Add the dataset name to the column names of the new dataframe to avoid duplicates and modify the column names acting as keys in the new_dataframe_tags dictionary
+                        for duplicate_column_name in intersection.keys():
+                            new_dataframe.rename(
+                                columns={duplicate_column_name: duplicate_column_name + "_" + dataset_name_without_ext}, inplace=True)
+                            new_dataframe_tags[duplicate_column_name + "_" + dataset_name_without_ext] = new_dataframe_tags.pop(duplicate_column_name)
                 # Merge the dataset
                 first_dataset = first_dataset.merge(
                     new_dataframe, how=dataset_merge_type, on=dataset_merge_on)
 
+                # Update the tags
+                tags.update(new_dataframe_tags)
+
+
+
         # Save the merged dataset
         final_dataset_extension = json_config["finalDatasetExtension"]
         final_dataset_path = json_config["finalDatasetPath"]
-        save_dataframe(final_dataset_path, final_dataset_extension, first_dataset)
+        save_dataframe(final_dataset_path, final_dataset_extension, first_dataset, tags=tags)
         self.set_progress(now=100, label="Done, saving the file at " + final_dataset_path)
 
         json_config["final_path"] = final_dataset_path
