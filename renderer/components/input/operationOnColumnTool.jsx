f1fc25ae83acaa91381d93e75c47def16bde8a48
Sari27 <sarah.dns.sd@gmail.com>
2024-04-25 15:06:28 -0400

    Added operationOnColumnTool which replace deleteColumnsTool and transformColumnsTool

--- renderer/components/input/operationOnColumnTool.jsx
+++ renderer/components/input/operationOnColumnTool.jsx
@@ -1,13 +1,10 @@
-import React, { useContext, useState, useEffect } from "react"
+import React, { useState, useEffect, useContext } from "react"
 import { Row, Col } from "react-bootstrap"
-import { DataContext } from "../workspace/dataContext"
 import { Dropdown } from "primereact/dropdown"
-import MedDataObject from "../workspace/medDataObject"
 import { DataTable } from "primereact/datatable"
 import { Column } from "@blueprintjs/table"
-import { toast } from "react-toastify"
 import { MultiSelect } from "primereact/multiselect"
-import SaveDataset from "../generalPurpose/saveDataset"
+import { DataContext } from "../workspace/dataContext"
 import {
   cleanDataset,
   generateHeader,
@@ -19,20 +16,21 @@ import {
   updateListOfDatasets,
   updateTheColumnsTypes
 } from "./simpleToolsUtils"
+import MedDataObject from "../workspace/medDataObject"
+import { toast } from "react-toastify"
+import SaveDataset from "../generalPurpose/saveDataset"
 
 const dfd = require("danfojs-node")
 
 /**
- * Component that renders the transform column tool
+ * Component that renders the delete columns tool
  */
-const TransformColumnsTool = () => {
+const OperationOnColumnTool = ({ operationType }) => {
   const { globalData } = useContext(DataContext) // The global data object
   const [listOfDatasets, setListOfDatasets] = useState([]) // The list of datasets
   const [selectedDataset, setSelectedDataset] = useState(null) // The selected dataset
-
   const [newDatasetName, setNewDatasetName] = useState("") // The name of the new dataset
   const [newDatasetExtension, setNewDatasetExtension] = useState("csv") // The extension of the new dataset
-
   const [selectedDatasetColumns, setSelectedDatasetColumns] = useState([]) // The columns infos of the selected dataset
   const opTab = React.useRef(null)
   const [dataset, setDataset] = useState(null) // The dataset to drop
@@ -40,9 +38,8 @@ const TransformColumnsTool = () => {
   const [columnTypes, setColumnTypes] = useState({}) // The column types
   const [selectedColumns, setSelectedColumns] = useState([]) // The selected columns
   const [selectedColumnsOptions, setSelectedColumnsOptions] = useState([]) // The selected columns options
-  const [visibleColumns, setVisibleColumns] = useState([])
-
   const [transformType, setTransformType] = useState("binary_0_1") // The type of transformation
+  const [visibleColumns, setVisibleColumns] = useState([])
 
   const transformOptions = [
     { label: "Binary : NaN values become 0, non-NaN values become 1", value: "binary_0_1" },
@@ -63,6 +60,7 @@ const TransformColumnsTool = () => {
     if (selectedDataset) {
       getData(selectedDataset).then((data) => {
         data = cleanDataset(data)
+
         let columns = data.columns
         setDf(data)
         let jsonData = dfd.toJSON(data)
@@ -77,7 +75,7 @@ const TransformColumnsTool = () => {
         setVisibleColumns(newSelectedColumns)
       })
       setNewDatasetExtension(selectedDataset.extension)
-      setNewDatasetName(selectedDataset.nameWithoutExtension + "_transformed_cols")
+      setNewDatasetName(selectedDataset.nameWithoutExtension + "_" + operationType + "_cols")
     } else {
       setDf(null)
       setDataset(null)
@@ -90,21 +88,59 @@ const TransformColumnsTool = () => {
     }
   }, [selectedDataset])
 
+  const renderHeader = () => {
+    return (
+      <div className="table-header" style={{ display: "flex", justifyContent: "flex-start", alignItems: "center" }}>
+        {/* Add a label for the multiselect : Toggle columns */}
+        <label htmlFor="toggleColumns" className="p-checkbox-label" style={{ marginLeft: "0.5rem" }}>
+          {operationType == "delete" && <>Select the columns to keep: &nbsp;</>}
+          {operationType == "transform" && <>Select the columns to transform: &nbsp;</>}
+        </label>
+        {/* Add the multiselect to select the columns to display */}
+        <MultiSelect
+          value={selectedColumnsOptions}
+          options={selectedColumns}
+          onChange={(e) => {
+            let newSelectedColumns = e.value.map((value) => {
+              return { name: value, value: value }
+            })
+            let orderedSelectedColumns = selectedColumns.filter((col) => newSelectedColumns.some((sCol) => sCol.value === col.value))
+            setVisibleColumns(orderedSelectedColumns)
+            setSelectedColumnsOptions(e.value)
+          }}
+          optionLabel="name"
+          placeholder="Select columns to display"
+          display="chip"
+          style={{ maxWidth: "50%" }}
+        />
+      </div>
+    )
+  }
+
   /**
    * To apply the selected filter and save the filtered dataset
    * @param {Boolean} overwrite - True if the dataset should be overwritten, false otherwise
    */
   const saveFilteredDataset = (overwrite = false) => {
-    let newData = df.copy()
-    let columnsToTransform = selectedColumnsOptions
-    columnsToTransform.forEach((column) => {
-      if (transformType === "binary_0_1") {
-        newData[column] = newData[column].apply((x) => (x === null || x === undefined || x === "NaN" ? 0 : 1))
-      } else if (transformType === "nan_to_zero") {
-        newData[column] = newData[column].apply((x) => (x === null || x === undefined || x === "NaN" ? 0 : x))
-      }
-    })
-    if (newData && newData.length !== 0 && columnsToTransform.length > 0) {
+    // Call the function depending on the operation type choosen
+    let newData = null
+    let columnsToOperate = []
+    if (operationType == "delete") {
+      columnsToOperate = selectedColumns.filter((col) => !visibleColumns.some((vCol) => vCol.name === col.name)).map((col) => col.name)
+      newData = df.drop({ columns: columnsToOperate })
+    } else if (operationType == "transform") {
+      newData = df.copy()
+      columnsToOperate = selectedColumns.filter((col) => visibleColumns.some((vCol) => vCol.name === col.name)).map((col) => col.name)
+      columnsToOperate.forEach((column) => {
+        if (transformType === "binary_0_1") {
+          newData[column] = newData[column].apply((x) => (x === null || x === undefined || x === "NaN" ? 0 : 1))
+        } else if (transformType === "nan_to_zero") {
+          newData[column] = newData[column].apply((x) => (x === null || x === undefined || x === "NaN" ? 0 : x))
+        }
+      })
+    }
+    // Save the data
+    if (newData?.$data?.length > 0 && newData?.$columns?.length > 0 && columnsToOperate.length > 0) {
       if (overwrite) {
         MedDataObject.saveDatasetToDisk({
           df: newData,
@@ -122,36 +158,8 @@ const TransformColumnsTool = () => {
 
       MedDataObject.updateWorkspaceDataObject()
     } else {
-      toast.error("No columns to transform")
+      toast.error("Operation on column(s) failed")
     }
-  }
-
-  const renderHeader = () => {
-    return (
-      <div className="table-header" style={{ display: "flex", justifyContent: "flex-start", alignItems: "center" }}>
-        {/* Add a label for the multiselect : Toggle columns */}
-        <label htmlFor="toggleColumns" className="p-checkbox-label" style={{ marginLeft: "0.5rem" }}>
-          Select the columns to transform: &nbsp;
-        </label>
-        {/* Add the multiselect to select the columns to display */}
-        <MultiSelect
-          value={selectedColumnsOptions}
-          options={selectedColumns}
-          onChange={(e) => {
-            let newSelectedColumns = e.value.map((value) => {
-              return { name: value, value: value }
-            })
-            let orderedSelectedColumns = selectedColumns.filter((col) => newSelectedColumns.some((sCol) => sCol.value === col.value))
-            setVisibleColumns(orderedSelectedColumns)
-            setSelectedColumnsOptions(e.value)
-          }}
-          optionLabel="name"
-          placeholder="Select columns to transform"
-          display="chip"
-          style={{ maxWidth: "50%" }}
-        />
-      </div>
-    )
   }
 
   /**
@@ -218,22 +226,24 @@ const TransformColumnsTool = () => {
               <b>{selectedColumns ? selectedColumns.length : 0}</b>
             </h6>
           </Row>
-          <Row
-            className={"card"}
-            style={{
-              display: "flex",
-              justifyContent: "space-evenly",
-              flexDirection: "row",
-              marginTop: "0.5rem",
-              backgroundColor: "transparent",
-              padding: "0.5rem"
-            }}
-          >
-            <label>
-              <strong>Choose the type of transformation</strong>
-            </label>
-            <Dropdown value={transformType} options={transformOptions} onChange={(e) => setTransformType(e.target.value)} placeholder="Select the type of transformation" />
-          </Row>
+          {operationType == "transform" && (
+            <Row
+              className={"card"}
+              style={{
+                display: "flex",
+                justifyContent: "space-evenly",
+                flexDirection: "row",
+                marginTop: "0.5rem",
+                backgroundColor: "transparent",
+                padding: "0.5rem"
+              }}
+            >
+              <label>
+                <strong>Choose the type of transformation</strong>
+              </label>
+              <Dropdown value={transformType} options={transformOptions} onChange={(e) => setTransformType(e.target.value)} placeholder="Select the type of transformation" />
+            </Row>
+          )}
           <SaveDataset
             newDatasetName={newDatasetName}
             newDatasetExtension={newDatasetExtension}
@@ -248,4 +258,4 @@ const TransformColumnsTool = () => {
   )
 }
 
-export default TransformColumnsTool
+export default OperationOnColumnTool
