9cdacce1a9b4218b69c52d8bd92f16c9550f0616
Sarah DENIS <48992399+Sari27@users.noreply.github.com>
2024-04-25 09:01:24 -0400

    Fix loosing folders when drop them in the app (#274)
    Close #214
    Need to run npm install

--- renderer/components/mainPages/dataComponents/dropzoneComponent.jsx
+++ renderer/components/mainPages/dataComponents/dropzoneComponent.jsx
@@ -1,9 +1,10 @@
-import React, { useCallback, useState, useContext, useMemo } from "react"
+import React, { useCallback, useState, useContext } from "react"
 import { useDropzone } from "react-dropzone"
 import fs from "fs"
 import { WorkspaceContext } from "../../workspace/workspaceContext"
 import MedDataObject from "../../workspace/medDataObject"
 import { toast } from "react-toastify"
+import { getDroppedOrSelectedFiles } from "html5-file-selector"
 
 /**
  * @typedef {React.FunctionComponent} DropzoneComponent
@@ -12,11 +13,14 @@ import { toast } from "react-toastify"
  * @summary This component is used to upload files to the workspace. It is used in the InputSidebar.
  * @todo Add the functionality to upload more file types than just CSV files
  */
-export default function DropzoneComponent({ children, item = undefined, ...props }) {
-  // eslint-disable-next-line no-unused-vars
-  const [uploadedFile, setUploadedFile] = useState(null)
-  // eslint-disable-next-line no-unused-vars
-  const [uploadProgress, setUploadProgress] = useState(0)
+export default function DropzoneComponent({ children, item = undefined, setIsDropping, ...props }) {
+  const [style, setStyle] = useState({
+    display: "block",
+    position: "relative",
+    width: "100%",
+    height: "100%",
+    borderWidth: "0px"
+  })
 
   const { workspace } = useContext(WorkspaceContext)
 
@@ -27,17 +31,83 @@ export default function DropzoneComponent({ children, item = undefined, ...props
     }
   }
 
-  const onDrop = useCallback((acceptedFiles, fileRejections, event) => {
-    console.log("event", event)
-    event.stopPropagation()
-    const reader = new FileReader()
+  let acceptedFiles = undefined
+  if (item !== undefined) {
+    if (item.acceptedFiles !== undefined) {
+      acceptedFiles = item.acceptedFiles
+    }
+  }
 
-    reader.onabort = () => console.log("file reading was aborted")
-    reader.onerror = () => console.log("file reading failed")
-    reader.onload = () => {
+  /**
+   * Splits the provided `string` at the last occurrence of the provided `separator`.
+   * @param {string} string - The string to split.
+   * @param {string} separator - The separator to split the string at.
+   * @returns {Array} - An array containing the first elements of the split string and the last element of the split string.
+   */
+  function splitStringAtTheLastSeparator(string, separator) {
+    let splitString = string.split(separator)
+    let lastElement = splitString.pop()
+    let firstElements = splitString.join(separator)
+    return [firstElements, lastElement]
+  }
+
+  /**
+   * @description This function is used to get the fullPath of the dragged files, in order to know if we drag only file(s) or a folder
+   * @param {Event} event
+   * @returns A list of File Objects with fullPath property
+   */
+  async function myCustomFileGetter(event) {
+    const fileObjects = []
+    const newDirectoriesNames = {}
+    const newFilesNames = {}
+    let files = await getDroppedOrSelectedFiles(event)
+    files.forEach((file) => {
+      let fileObject = file.fileObject
+      let fullPath = file.fullPath
+      let splittedPath = fullPath.split("/")
+      console.log("splitted path", splittedPath)
+      if (splittedPath.length > 2) {
+        // If we drop a folder
+        // Check if the name of the folder to drop is not already in the directory
+        let newName = MedDataObject.getNewNameForFolder({ name: splittedPath[1], folderPath: directoryPath + "/" })
+        if (newName != splittedPath[1]) {
+          newDirectoriesNames[splittedPath[1]] = newName
+          splittedPath[1] = newName
+          fullPath = splittedPath.join("/")
+        }
+      } else {
+        // If we drop a file
+        // Check if the name of the file to drop is not already in the directory
+        let extension = splitStringAtTheLastSeparator(fileObject.name, ".")[1]
+        let newName = MedDataObject.getNewNameForFile({ name: fileObject.name, folderPath: directoryPath + "/", extension: extension })
+        if (newName != fileObject.name) {
+          newFilesNames[fileObject.name] = newName
+          fullPath = fullPath.replace(fileObject.name, newName)
+        }
+      }
+      Object.defineProperty(fileObject, "fullPath", { value: fullPath })
+      fileObjects.push(fileObject)
+    })
+    // Set toast warnings if some names have been changed
+    for (const [oldName, newName] of Object.entries(newDirectoriesNames)) {
+      toast.warning(`Dropped directory ${oldName} renamed to ${newName}`)
+    }
+    for (const [oldName, newName] of Object.entries(newFilesNames)) {
+      toast.warning(`Dropped file ${oldName} renamed to ${newName}`)
+    }
+    return fileObjects
+  }
+
+  /**
+   * @description The function to be executed when a file is dropped in the dropzone
+   */
+  const onDrop = useCallback((acceptedFiles) => {
+    console.log("accepted files", acceptedFiles)
+    if (acceptedFiles && acceptedFiles.length > 0) {
       acceptedFiles.forEach((file) => {
-        console.log("file", file)
-        fs.copyFile(file.path, `${directoryPath}/${file["name"]}`, (err) => {
+        let firstElements = splitStringAtTheLastSeparator(file.fullPath, "/")[0]
+        MedDataObject.createFolderFromPath(`${directoryPath}/${firstElements}`)
+        fs.copyFile(file.path, `${directoryPath}/${file.fullPath}`, (err) => {
           if (err) {
             console.error("Error copying file:", err)
           } else {
@@ -45,20 +115,43 @@ export default function DropzoneComponent({ children, item = undefined, ...props
           }
         })
       })
-    }
-
-    // read file contents
-    acceptedFiles.forEach((file) => {
-      reader.readAsBinaryString(file)
+      console.log("UPDATE")
       MedDataObject.updateWorkspaceDataObject()
-    })
+    }
+    setIsDropping(false)
+    setStyle({ ...style, ...baseStyle })
   }, [])
 
-  let acceptedFiles = undefined
-  if (item !== undefined) {
-    if (item.acceptedFiles !== undefined) {
-      acceptedFiles = item.acceptedFiles
-    }
+  /**
+   * @description The function to be executed when a file is dropped in the dropzone but is rejected
+   */
+  const onDropRejected = useCallback((fileRejections) => {
+    console.log("fileRejections", fileRejections)
+    fileRejections.forEach((rejection) => {
+      if (rejection.file && rejection.file.name) {
+        if (rejection.errors && rejection.errors.length > 0 && rejection.errors[0].message) {
+          toast.error(rejection.file.name + " rejected: " + rejection.errors[0].message)
+        } else {
+          toast.error(rejection.file.name + " rejected: File type not accepted in this folder")
+        }
+      } else {
+        toast.error("File type not accepted in this folder")
+      }
+    })
+    setIsDropping(false)
+  }, [])
+
+  // Event handler for dropzone hover
+  const onDragOver = (event) => {
+    event.preventDefault()
+    setIsDropping(true)
+    setStyle({ ...style, ...focusStyle })
+  }
+  // Event handler for dropzone hover leave
+  const onDragLeave = (event) => {
+    event.preventDefault()
+    setIsDropping(false)
+    setStyle({ ...style, ...baseStyle })
   }
 
   /**
@@ -70,17 +163,18 @@ export default function DropzoneComponent({ children, item = undefined, ...props
    * @returns {JSX.Element}
    * @see SidebarDirectoryTreeControlled - "../../layout/sidebarTools/sidebarDirectoryTreeControlled.jsx" This component is used in the SidebarDirectoryTreeControlled component
    */
-  const { getRootProps, getInputProps, isFocused, isDragAccept, isDragReject } = useDropzone({
+  const { getRootProps, getInputProps } = useDropzone({
     onDrop,
-    onDropRejected: useCallback((fileRejections) => {
-      console.log("fileRejections", fileRejections)
-      toast.error("Error: File type not accepted in this folder")
-    }, []),
+    onDropRejected,
+    getFilesFromEvent: (event) => myCustomFileGetter(event),
+    onDragOver,
+    onDragLeave,
     noClick: props.noClick || false,
-    accept: acceptedFiles ? acceptedFiles : undefined
+    accept: acceptedFiles ? acceptedFiles : undefined,
+    noDragEventsBubbling: true
   })
 
-  // The style changes if the dropzone is focused, if the file is accepted or if the file is rejected
+  // The style changes if the dropzone is focused
   const baseStyle = {
     display: "block",
     position: "relative",
@@ -89,27 +183,10 @@ export default function DropzoneComponent({ children, item = undefined, ...props
     borderWidth: "0px"
   }
 
-  const focusedStyle = {}
-
-  const acceptStyle = {
+  const focusStyle = {
     borderWidth: "2px",
-    borderColor: "#00e676"
+    borderColor: "#FFFFFF"
   }
-
-  const rejectStyle = {
-    borderWidth: "2px",
-    borderColor: "#ff1744"
-  }
-
-  const style = useMemo(
-    () => ({
-      ...baseStyle,
-      ...(isFocused ? focusedStyle : {}),
-      ...(isDragAccept ? acceptStyle : {}),
-      ...(isDragReject ? rejectStyle : {})
-    }),
-    [isFocused, isDragAccept, isDragReject]
-  )
 
   return (
     <div style={{ display: "block" }}>
