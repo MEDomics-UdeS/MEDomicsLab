b3c874c7e52fe70fedc230a16eb38b8a25b4fcd7
MahdiAll99 <mehdiwinneri01@gmail.com>
2024-05-20 15:38:21 -0400

    Added new routes and new handle functions

--- go_server/blueprints/extractionMEDimage/extractionMEDimage.go
+++ go_server/blueprints/extractionMEDimage/extractionMEDimage.go
@@ -9,8 +9,10 @@ var prePath = "extraction_MEDimage"
 
 // AddHandleFunc adds the specific module handle function to the server
 func AddHandleFunc() {
-	Utils.CreateHandleFunc(prePath+"/run_all", handleRunAll)
-	Utils.CreateHandleFunc(prePath+"/upload", handleGetUpload)
+	Utils.CreateHandleFunc(prePath+"/run_all/", handleRunAll)
+	Utils.CreateHandleFunc(prePath+"/upload/", handleGetUpload)
+	Utils.CreateHandleFunc(prePath+"/progress/", handleProgress)
+	Utils.CreateHandleFunc(prePath+"/view/", handleGetView)
 }
 
 // handleRunAll handles the request to run extraction using all nodes
@@ -24,7 +26,7 @@ func handleRunAll(jsonConfig string, id string) (string, error) {
 	return response, nil
 }
 
-// handleGetUpload handles the request to run extraction using all nodes
+// handleGetUpload handles the request to upload a scan/image to the server
 // It returns the response from the python script
 func handleGetUpload(jsonConfig string, id string) (string, error) {
 	log.Println("Running get upload for MEDimage extraction", id)
@@ -33,4 +35,28 @@ func handleGetUpload(jsonConfig string, id string) (string, error) {
 		return "", err
 	}
 	return response, nil
+}
+
+// handleGetView handles the request to view the scan/image uploaded to the server
+// It returns the response from the python script
+func handleGetView(jsonConfig string, id string) (string, error) {
+	log.Println("Running get upload for MEDimage extraction")
+	response, err := Utils.StartPythonScripts(jsonConfig, "../pythonCode/modules/extraction_MEDimage/get_view.py", "extractionMEDimage")
+	if err != nil {
+		return "", err
+	}
+	return response, nil
+}
+
+// handleProgress handles the request to get the progress of the experiment
+// It returns the progress of the experiment
+func handleProgress(jsonConfig string, id string) (string, error) {
+	Utils.Mu.Lock()
+	progress := Utils.Scripts[id].Progress
+	Utils.Mu.Unlock()
+	if progress != "" {
+		return progress, nil
+	} else {
+		return "{\"now\":\"0\", \"currentLabel\":\"Warming up\"}", nil
+	}
 }
--- go_server/blueprints/learningMEDimage/learningMEDimage.go
+++ go_server/blueprints/learningMEDimage/learningMEDimage.go
@@ -9,7 +9,8 @@ var prePath = "learning_MEDimage"
 
 // AddHandleFunc adds the specific module handle function to the server
 func AddHandleFunc() {
-	Utils.CreateHandleFunc(prePath+"/run_all", handleRunExperiment)
+	Utils.CreateHandleFunc(prePath+"/run_all/", handleRunExperiment)
+	Utils.CreateHandleFunc(prePath+"/progress/", handleProgress)
 }
 
 // handleRunExperiment handles the request to run an experiment
@@ -21,4 +22,17 @@ func handleRunExperiment(jsonConfig string, id string) (string, error) {
 		return "", err
 	}
 	return response, nil
+}
+
+// handleProgress handles the request to get the progress of the experiment
+// It returns the progress of the experiment
+func handleProgress(jsonConfig string, id string) (string, error) {
+	Utils.Mu.Lock()
+	progress := Utils.Scripts[id].Progress
+	Utils.Mu.Unlock()
+	if progress != "" {
+		return progress, nil
+	} else {
+		return "{\"now\":\"0\", \"currentLabel\":\"Warming up\"}", nil
+	}
 }
