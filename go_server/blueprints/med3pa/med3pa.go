64d721ab3bdf4bcd1bfb4d9759fa3298f5a6bd50
ludmila <jl_amriou@esi.dz>
2024-07-08 07:59:18 +0100

    Add: Get MED3paParams nodes From Backend + Filter with MaxDepth

--- go_server/blueprints/med3pa/med3pa.go
+++ go_server/blueprints/med3pa/med3pa.go
@@ -9,8 +9,13 @@ var prePath = "med3pa"
 
 // AddHandleFunc adds the specific module handle function to the server
 func AddHandleFunc() {
+	
 	Utils.CreateHandleFunc(prePath+"/hello_world/", handleHelloWorld)
 	Utils.CreateHandleFunc(prePath+"/progress/", handleProgress)
+	Utils.CreateHandleFunc(prePath+"/send_params/", handleMed3paParams)
+//	Utils.CreateHandleFunc(prePath+"/run_experiments/", handleMed3paRunPipeline)
+
+
 }
 
 // handleStartSweetviz handles the request to run a sweetviz analysis
@@ -24,6 +29,26 @@ func handleHelloWorld(jsonConfig string, id string) (string, error) {
 	}
 	return response, nil
 }
+
+func handleMed3paParams(jsonConfig string, id string) (string, error) {
+	log.Println("Running Experiment...", id)
+	response, err := Utils.StartPythonScripts(jsonConfig, "../pythonCode/modules/med3pa/med3pa_params.py", id)
+	Utils.RemoveIdFromScripts(id)
+	if err != nil {
+		return "", err
+	}
+	return response, nil
+}
+
+// func handleMed3paRunPipeline(jsonConfig string, id string) (string, error) {
+// 	log.Println("Running Experiment...", id)
+// 	response, err := Utils.StartPythonScripts(jsonConfig, "../pythonCode/modules/med3pa/run_experiments.py", id)
+// 	Utils.RemoveIdFromScripts(id)
+// 	if err != nil {
+// 		return "", err
+// 	}
+// 	return response, nil
+// }
 
 // handleProgress handles the request to get the progress of the experiment
 // It returns the progress of the experiment
