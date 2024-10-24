b70eb2edf7fddfafc607ec6ccac41820215466b6
NicoLongfield <lonn1001@usherbrooke.ca>
2024-10-24 00:53:45 -0400

    Error with launching the app Building for windows only - fourth try

--- utilScripts/build_preparation_win.bat
+++ utilScripts/build_preparation_win.bat
@@ -1,4 +1,4 @@
 
-powershell -Command "(Get-Content -Path 'medomics.dev.js') -replace 'runServerAutomatically: false', 'runServerAutomatically: true' | Set-Content -Path 'medomics.dev.js'"
+@REM powershell -Command "(Get-Content -Path 'medomics.dev.js') -replace 'runServerAutomatically: false', 'runServerAutomatically: true' | Set-Content -Path 'medomics.dev.js'"
 
 %~dp0pack_GO.bat
