490f6b03086f08fba79cec25e49a457d55731ce8
MahdiAll99 <mehdiwinneri01@gmail.com>
2025-01-06 12:59:20 -0500

    Implemented loading status for all buttons, plus some code cleaning and minor improvements

--- renderer/components/dbComponents/inputToolsDB/holdoutSetCreationToolsDB.jsx
+++ renderer/components/dbComponents/inputToolsDB/holdoutSetCreationToolsDB.jsx
@@ -1,22 +1,23 @@
 /* eslint-disable no-unused-vars */
-import React, { useContext, useEffect, useState } from "react"
-import { Checkbox } from "primereact/checkbox"
-import { MultiSelect } from "primereact/multiselect"
-import { Message } from "primereact/message"
-import { getCollectionData } from "../utils"
-import { InputNumber } from "primereact/inputnumber"
+import { randomUUID } from "crypto"
 import { ipcRenderer } from "electron"
-import { Slider } from "primereact/slider"
-import { Dropdown } from "primereact/dropdown"
-import { InputText } from "primereact/inputtext"
 import { Button } from "primereact/button"
-import { requestBackend } from "../../../utilities/requests"
-import { ServerConnectionContext } from "../../serverConnection/connectionContext"
+import { Checkbox } from "primereact/checkbox"
+import { confirmDialog } from "primereact/confirmdialog"
+import { Dropdown } from "primereact/dropdown"
+import { InputNumber } from "primereact/inputnumber"
+import { InputText } from "primereact/inputtext"
+import { Message } from "primereact/message"
+import { MultiSelect } from "primereact/multiselect"
+import { Slider } from "primereact/slider"
+import React, { useContext, useEffect, useState } from "react"
 import { toast } from "react-toastify"
+import { requestBackend } from "../../../utilities/requests"
+import { insertMEDDataObjectIfNotExists } from "../../mongoDB/mongoDBUtils"
+import { ServerConnectionContext } from "../../serverConnection/connectionContext"
 import { MEDDataObject } from "../../workspace/NewMedDataObject"
 import { DataContext } from "../../workspace/dataContext"
-import { randomUUID } from "crypto"
-import { insertMEDDataObjectIfNotExists } from "../../mongoDB/mongoDBUtils"
+import { getCollectionData } from "../utils"
 
 /**
  * @description
@@ -36,6 +37,7 @@ const HoldoutSetCreationToolsDB = ({ refreshData, currentCollection }) => {
   const [cleaningOption, setCleaningOption] = useState("drop")
   const cleaningOptions = ["drop", "random fill", "mean fill", "median fill", "mode fill", "bfill", "ffill"]
   const [newCollectionName, setNewCollectionName] = useState("")
+  const [loading, setLoading] = useState(false)
   const { globalData } = useContext(DataContext)
   const { port } = useContext(ServerConnectionContext)
 
@@ -64,22 +66,23 @@ const HoldoutSetCreationToolsDB = ({ refreshData, currentCollection }) => {
    * @async
    */
   const createHoldoutSet = async () => {
+    let collectionName = newCollectionName + ".csv"
     const id = randomUUID()
     const id2 = randomUUID()
     const object = new MEDDataObject({
       id: id,
-      name: "Learning_" + newCollectionName,
+      name: "Learning_" + collectionName,
       type: "csv",
-      parentID: "ROOT",
+      parentID: globalData[currentCollection].parentID,
       childrenIDs: [],
       inWorkspace: false
     })
 
     const object2 = new MEDDataObject({
       id: id2,
-      name: "Holdout_" + newCollectionName,
+      name: "Holdout_" + collectionName,
       type: "csv",
-      parentID: "ROOT",
+      parentID: globalData[currentCollection].parentID,
       childrenIDs: [],
       inWorkspace: false
     })
@@ -102,14 +105,31 @@ const HoldoutSetCreationToolsDB = ({ refreshData, currentCollection }) => {
     let exists = false
     for (const item of Object.keys(globalData)) {
       if (globalData[item].name && globalData[item].name === object.name) {
-        toast.warn("Files with the same name already exist, consider changing the name or deleting the existing files")
         exists = true
         break
       }
     }
+
+    // If the collection already exists, ask the user if they want to overwrite it
+    let overwriteConfirmation = true
     if (exists) {
+      overwriteConfirmation = await new Promise((resolve) => {
+        confirmDialog({
+          closable: false,
+          message: `A dataset with the name "${collectionName}" already exists in the database. Do you want to overwrite it?`,
+          header: "Confirmation",
+          icon: "pi pi-exclamation-triangle",
+          accept: () => resolve(true),
+          reject: () => resolve(false)
+        })
+      })
+    }
+    if (!overwriteConfirmation) {
       return
     }
+
+    // Change loading state
+    setLoading(true)
 
     // Send the request to the backend
     requestBackend(
@@ -117,6 +137,7 @@ const HoldoutSetCreationToolsDB = ({ refreshData, currentCollection }) => {
       "/input/create_holdout_set_DB/",
       JSONToSend,
       async (jsonResponse) => {
+        setLoading(false)
         console.log("jsonResponse", jsonResponse)
         if (jsonResponse.error) {
           if (jsonResponse.error.message) {
@@ -134,6 +155,7 @@ const HoldoutSetCreationToolsDB = ({ refreshData, currentCollection }) => {
         }
       },
       (error) => {
+        setLoading(false)
         console.log(error)
         toast.error("Error cleaning data:" + error)
       }
@@ -166,8 +188,8 @@ const HoldoutSetCreationToolsDB = ({ refreshData, currentCollection }) => {
           }
         />
         <Message className="margin-top-15 margin-bottom-15 center" severity="success" text={`Current collection: ${globalData[currentCollection].name}`} style={{ marginTop: "10px" }} />
-        <div style={{ marginTop: "10px", marginLeft: "80px" }}>
-          <div style={{ marginTop: "10px", display: "flex", justifyContent: "center", marginRight: "70px", alignItems: "center" }}>
+        <div style={{ marginTop: "10px", justifyContent: "center", alignItems: "center"}}>
+          <div style={{ marginTop: "10px", display: "flex", justifyContent: "center", alignItems: "center" }}>
             <Checkbox
               inputId="shuffle"
               checked={shuffle}
@@ -207,7 +229,7 @@ const HoldoutSetCreationToolsDB = ({ refreshData, currentCollection }) => {
               style={{ marginLeft: "30px", marginTop: "10px" }}
             />
           </div>
-          <div style={{ marginTop: "10px", display: "flex", justifyContent: "center", marginRight: "70px", alignItems: "center" }}>
+          <div style={{ marginTop: "10px", display: "flex", justifyContent: "center", alignItems: "center" }}>
             <InputNumber
               value={seed}
               inputId="seed"
@@ -248,7 +270,7 @@ const HoldoutSetCreationToolsDB = ({ refreshData, currentCollection }) => {
               style={{ flex: "1 1 auto", width: "1px" }}
             />
           </div>
-          <div style={{ marginTop: "10px", justifyContent: "center", marginRight: "70px" }}>
+          <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
             <Dropdown
               value={cleaningOption}
               options={cleaningOptions}
@@ -257,24 +279,22 @@ const HoldoutSetCreationToolsDB = ({ refreshData, currentCollection }) => {
               tooltipOptions={{ position: "top" }}
               style={{ margin: "10px" }}
             />
-            <InputText
-              value={newCollectionName}
-              onChange={(e) => setNewCollectionName(e.target.value)}
-              placeholder="Holdout set name"
-              style={{
-                margin: "10px",
-                fontSize: "1rem",
-                width: "160px",
-                marginTop: "20px"
-              }}
-            />
+            <div className="p-inputgroup w-full md:w-30rem" style={{ margin: "10px", fontSize: "1rem", width: "230px", marginTop: "5px" }}>
+              <InputText
+                value={newCollectionName}
+                onChange={(e) => setNewCollectionName(e.target.value)}
+                placeholder="New set name"
+              />
+              <span className="p-inputgroup-addon">.csv</span>
+            </div>
             <Button
               icon="pi pi-plus"
-              style={{ margin: "10px", fontSize: "1rem", marginTop: "20px" }}
+              style={{ margin: "10px"}}
               onClick={() => {
                 createHoldoutSet()
               }}
-              tooltip="Create holdout set "
+              loading={loading}
+              tooltip="Create holdout set"
               tooltipOptions={{ position: "top" }}
               disabled={newCollectionName === ""}
             />
