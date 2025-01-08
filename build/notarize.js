c4152e9441e147a0455797742a05804a50091f19
NicoLongfield <lonn1001@usherbrooke.ca>
2025-01-08 18:28:39 -0500

    Adjustments for github actions

--- build/notarize.js
+++ build/notarize.js
@@ -1,20 +1,25 @@
-// require('dotenv').config();
-// const { notarize } = require('electron-notarize');
+require('dotenv').config();
+const { notarize } = require('@electron/notarize');
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;  
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
 
-// exports.default = async function notarizing(context) {
-//   const { electronPlatformName, appOutDir } = context;  
-//   if (electronPlatformName !== 'darwin') {
-//     return;
-//   }
+  const appName = context.packager.appInfo.productFilename;
+  console.log(`Notarizing ${appName}... at ${appOutDir}/${appName}.pkg`);
 
-//   const appName = context.packager.appInfo.productFilename;
+  console.log('Notarizing... with process.env.APPLE_TEAM_ID:', process.env.APPLE_TEAM_ID);
+  console.log('Notarizing... with process.env.APPLE_NOTARY_USER:', process.env.APPLE_NOTARY_USER);
+  console.log('Notarizing... with process.env.APPLE_NOTARY_PASSWORD:', process.env.APPLE_NOTARY_PASSWORD);
 
-//   return await notarize({
-//     tool: 'notarytool',
-//     teamId: process.env.APPLETEAMID,
-//     appBundleId: 'com.yourcompany.yourAppId',
-//     appPath: `${appOutDir}/${appName}.app`,
-//     appleId: process.env.APPLEID,
-//     appleIdPassword: process.env.APPLEIDPASS,
-//   });
-// };
+  return await notarize({
+    tool: 'notarytool',
+    teamId: process.env.APPLE_TEAM_ID,
+    appBundleId: 'com.medomicslab.medapp',
+    // /Users/nicolas/Documents/GitHub/MEDomicsLab_Automation/build/dist/MEDomicsLab-1.0.0-mac.pkg
+    appPath: `/Users/nicolas/Documents/GitHub/MEDomicsLab_Automation/build/dist/MEDomicsLab-1.0.0-mac.pkg`,
+    appleId: process.env.APPLE_NOTARY_USER,
+    appleIdPassword: process.env.APPLE_NOTARY_PASSWORD,
+  });
+};
