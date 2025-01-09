6a89d3ca1a1f30737783099f414260cbe01ef86b
NicoLongfield <lonn1001@usherbrooke.ca>
2025-01-08 23:57:00 -0500

    Adjustments for github actions2

--- build/notarize.js
+++ build/notarize.js
@@ -7,7 +7,7 @@ exports.default = async function notarizing(context) {
   }
 
   const appName = context.packager.appInfo.productFilename;
-
+  
   console.log('Notarizing... with process.env.APPLE_TEAM_ID:', process.env.APPLE_TEAM_ID);
   console.log('Notarizing... with process.env.APPLE_NOTARY_USER:', process.env.APPLE_NOTARY_USER);
   console.log('Notarizing... with process.env.APPLE_NOTARY_PASSWORD:', process.env.APPLE_NOTARY_PASSWORD);
@@ -17,7 +17,7 @@ exports.default = async function notarizing(context) {
     teamId: process.env.APPLE_TEAM_ID,
     appBundleId: 'com.medomicslab.medapp',
     // /Users/nicolas/Documents/GitHub/MEDomicsLab_Automation/build/dist/MEDomicsLab-1.0.0-mac.pkg
-    appPath: process.env.FILE_NAME,
+    appPath: process.env.PWD2+process.env.FILE_NAME,
     appleId: process.env.APPLE_NOTARY_USER,
     appleIdPassword: process.env.APPLE_NOTARY_PASSWORD,
   });
