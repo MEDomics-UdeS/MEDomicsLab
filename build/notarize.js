f2b7ed49692f08178a95ba6775a76a636048035e
NicoLongfield <lonn1001@usherbrooke.ca>
2025-01-13 17:47:18 -0500

    Custom notarizing in build

--- build/notarize.js
+++ build/notarize.js
@@ -1,24 +1,30 @@
-require('dotenv').config();
-const { notarize } = require('@electron/notarize');
+const { notarize } = require("@electron/notarize");
+
 exports.default = async function notarizing(context) {
-  const { electronPlatformName, appOutDir } = context;  
-  if (electronPlatformName !== 'darwin') {
+  const appName = context.packager.appInfo.productFilename;
+  const { electronPlatformName, appOutDir } = context;
+  // We skip notarization if the process is not running on MacOS and
+  // if the enviroment variable SKIP_NOTARIZE is set to `true`
+  // This is useful for local testing where notarization is useless
+  if (
+    electronPlatformName !== "darwin" ||
+    process.env.SKIP_NOTARIZE === "true"
+  ) {
+    console.log(`  • Skipping notarization`);
     return;
   }
+  const appId = "com.medomicslab.medapp";
 
-  const appName = context.packager.appInfo.productFilename;
-  
-  console.log('Notarizing... with process.env.APPLE_TEAM_ID:', process.env.APPLE_TEAM_ID);
-  console.log('Notarizing... with process.env.APPLE_NOTARY_USER:', process.env.APPLE_NOTARY_USER);
-  console.log('Notarizing... with process.env.APPLE_NOTARY_PASSWORD:', process.env.APPLE_NOTARY_PASSWORD);
-  process.env.FILE_NAME
+  let appPath = `${appOutDir}/${appName}.app`;
+  let { APPLE_ID, APPLE_ID_PASSWORD, APPLE_TEAM_ID } = process.env;
+  console.log(`  • Notarizing ${appPath}`);
+
   return await notarize({
-    tool: 'notarytool',
-    teamId: process.env.APPLE_TEAM_ID,
-    appBundleId: 'com.medomicslab.medapp',
-    // /Users/nicolas/Documents/GitHub/MEDomicsLab_Automation/build/dist/MEDomicsLab-1.0.0-mac.pkg
-    appPath: process.env.PWD2+process.env.FILE_NAME,
-    appleId: process.env.APPLE_NOTARY_USER,
-    appleIdPassword: process.env.APPLE_NOTARY_PASSWORD,
+    tool: "notarytool",
+    appBundleId: appId,
+    appPath,
+    appleId: APPLE_ID,
+    appleIdPassword: APPLE_ID_PASSWORD,
+    teamId: APPLE_TEAM_ID,
   });
 };
